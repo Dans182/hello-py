3ec4fdec338e86846ce66c557634cb05b59dda00
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-07-21 09:25:34 +0200

    done

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-# Hola Mundo
+# Esto es un comentario
+# Nuestro hola mundo en python
 print("Hola Python")
+print('Hola Python')
+
+
+"""
+Esto es un comentario en 
+varias líneas
+"""
+
+'''
+Esto también es un 
+comentario en 
+varias líneas
+'''
