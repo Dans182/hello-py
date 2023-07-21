57d665bef15156c3047c5ea9f99432b9e0855489
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-07-21 10:34:11 +0200

    update 01

---
+++ 01-variables.py
@@ -0,0 +1,28 @@
+# Variables
+
+# por convención de py, recomiendan el uso de snake case
+my_string_variable = "My string variable"
+print(my_string_variable)
+
+my_int_variable = 8
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = False
+print(my_bool_variable)
+
+print(type(print("Mi cadena de texto"))) # no es un tipo de dato, es una funcion de sistema, por eso el NoneType
+
+# Concatenación de variables 
+print(my_string_variable, my_int_to_str_variable, my_bool_variable)
+print("Este es el valor de: ", my_bool_variable)
+
+# Algunas funciones de sistema
+print(len(my_string_variable))
+
+# Variables en una sola línea
+name, surname, alias, age = "Daniel", "Gaiteiro", "dans182", 33
+print(name, surname, alias, age)
