305601a626a5bc3300e03150d7be2e7d651a81de
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-07-28 11:54:39 +0200

    operadores

--- 02-operadores.py
+++ 02-operadores.py
@@ -1,1 +1,9 @@
-# Operadores
+### Operadores ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2) #operador de modulo. Te muestra el residuo en una división
+print(10 // 3) #Una división que se aproxima a un número entero. Redondea. 
+print(2 ** 3) #2 elevado a 3. Calcular exponentes
