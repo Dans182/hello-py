2a46c863ea0c0f6c4dc1f1fa6821d715d9cf0b9b
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-07-28 12:18:21 +0200

    operadores comparativos

--- 02-operadores.py
+++ 02-operadores.py
@@ -1,4 +1,4 @@
-### Operadores ###
+### Operadores Aritméticos ###
 
 print(3 + 4)
 print(3 - 4)
@@ -7,3 +7,28 @@ print(3 / 4)
 print(10 % 2) #operador de modulo. Te muestra el residuo en una división
 print(10 // 3) #Una división que se aproxima a un número entero. Redondea. 
 print(2 ** 3) #2 elevado a 3. Calcular exponentes
+
+print("Hola " + "Python") #el simbolo mas me sirve para concatenar strings
+print("Hola " * 4)
+
+### Operadores Comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+#Compara una ordenación alfabética por ASCII
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python")
+print("Hola" <= "Python")
+print("Hola" == "hola")
+print("Hola" != "Python")
+
+#Si quisieramos contar los caracteres tendríamos:
+print(len("aaa") >= len("abaa"))
+
+### Operadores Lógicos ###
