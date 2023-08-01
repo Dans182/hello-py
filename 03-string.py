32350a302140da81768a197da5c17dcf680eaffa
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-01 18:14:41 +0200

    doneç

--- 03-string.py
+++ 03-string.py
@@ -25,3 +25,21 @@ print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
 print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
 print("Mi nombre es: " + name + " " + surname + " y mi edad es: " + str(age))
 print(f"Mi nombre es {name} {surname} y mi edad es {age}")
+
+# Desempaquetado de caracteres
+
+language = "Python"
+a, b, c, d, e, f = language
+print(a)
+print(b)
+
+# División
+
+language_slice = language[1:3]
+print(language_slice)
+
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
