a71d6068a21161bb26b8306074b1280295c9ef97
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-01 17:52:57 +0200

    done

---
+++ 03-string.py
@@ -0,0 +1,18 @@
+### Strings ###
+
+my_string = "Mi String"
+my_other_string = 'Mi otro String'
+
+print(my_string)
+print(my_other_string)
+
+print(my_string + " " + my_other_string)
+
+my_new_line_string = 'Este es un String\ncon salto de línea'
+print(my_new_line_string)
+
+my_new_tab_string = '\tEste es un String con tabulación'
+print(my_new_tab_string)
+
+my_scape_string = '\\tEste es un String \nescapado'
+print(my_scape_string)
