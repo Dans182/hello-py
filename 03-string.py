d2f19cd9e74a2ccdf883f29961d5716ac2615265
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-01 18:07:40 +0200

    done

--- 03-string.py
+++ 03-string.py
@@ -16,3 +16,12 @@ print(my_new_tab_string)
 
 my_scape_string = '\\tEste es un String \nescapado'
 print(my_scape_string)
+
+# Formateo de cadenas de texto
+
+name, surname, age = "Daniel", "Gaiteiro", 33
+
+print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
+print("Mi nombre es %s %s y mi edad es %d" %(name, surname, age))
+print("Mi nombre es: " + name + " " + surname + " y mi edad es: " + str(age))
+print(f"Mi nombre es {name} {surname} y mi edad es {age}")
