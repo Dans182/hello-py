74fb6a8877f65d0c8164901f4f4d9e07825b42cd
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-10 18:23:39 +0200

    done

--- 09-loops.py
+++ 09-loops.py
@@ -7,19 +7,24 @@ my_condition = 0
 while my_condition < 10:
     print(my_condition) #si la condicion nunca cambia, es un bucle infinito
     my_condition += 2
-    #break
-if my_condition == 10:
+if my_condition == 10: #Y acá metes un if en el while. Y el else forma parte de la sintaxis del if y no del while. El elif no es compatible con el while.
     print("Mi condición es igual a 10")
 else:
     print("Mi condición es mayor o igual a 10")
 
-    print("La ejecución continúa")
+print("La ejecución continúa")
 
 # while my_condition < 10:
 #     print(my_condition)
 #     my_condition += 2
-#     #break
-# else: #Este else pertenece propiamente a la sintaxis del while, cosa que no es así en otros lenguajes
+# else: #Este else pertenece propiamente a la sintaxis del while, cosa que no es así en otros lenguajes. Es opcional
 #     print("Mi condición es mayor o igual a 10")
-
 #     print("La ejecución continúa")
+
+while my_condition < 20:
+    my_condition += 1
+    if my_condition == 15:
+        print("Mi condición es 15")
+        break
+
+    print(my_condition)
