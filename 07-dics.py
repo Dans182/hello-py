d6c90c89104a4017d60246a9f682014cf553e1e1
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-10 15:29:52 +0200

    done

--- 07-dics.py
+++ 07-dics.py
@@ -45,3 +45,14 @@ print(my_dict.values())
 
 my_new_dict = dict.fromkeys(("Nombre", 1)) #Esto me sirve para crear un nuevo directorio, con unos keys pero sin los valores
 print(my_new_dict)
+
+my_new_dict_2 = dict.fromkeys(my_other_dict, ("Roberto")) #Esto me sirve para duplicar los keys de otro diccionario en uno nuevo y a mayores, estoy seteando el valor de todos esos keys con UN valor idéntico para todos.
+
+my_values = my_new_dict_2.values()
+print("Dict", my_new_dict_2)
+print("Values", my_values)
+print("list", list(my_new_dict_2))
+print("Tuple", tuple(my_new_dict_2))
+print("Set", set(my_new_dict_2))
+
+print(type(my_values))
