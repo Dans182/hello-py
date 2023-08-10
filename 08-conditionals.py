7c753ea78cf89af616897877edd9fc955060c44d
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-10 17:06:10 +0200

    done

---
+++ 08-conditionals.py
@@ -0,0 +1,13 @@
+### Conditionals ###
+
+my_condition = False
+
+if my_condition: #Es lo mismo que esto if my_condition === True:
+    print("Se ejecuta la condición del if")
+
+my_condition = 5 * 2
+
+if my_condition: #Es lo mismo que esto if my_condition === True:
+    print("Se ejecuta la condición del segundo if") #Entra por acá y nos lo da como bueno, aunque no sea ni True ni False, simplemente porque tiene un dato dentro
+
+print("La ejecución continua")
