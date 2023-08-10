d6220abb081b1f3d5b60a77b9ca729c706a39688
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-10 15:07:12 +0200

    done

--- 07-dics.py
+++ 07-dics.py
@@ -5,3 +5,32 @@ my_other_dict = {}
 
 print(type(my_dict))
 print(type(my_other_dict))
+
+my_dict = {"Nombre":"Daniel", "Apellido": "Gaiteiro", "Edad":33, 1: "python"}
+
+my_other_dict = {"Nombre":"Daniel",
+                  "Apellido": "Gaiteiro", 
+                  "Edad":33,
+                  "Lenguajes": {"Javascript", "Python", "C++"},
+                    1: 1.81
+                }
+
+print(my_dict)
+print(my_other_dict)
+
+print(len(my_dict))
+print(len(my_other_dict))
+print(my_dict["Nombre"])
+
+my_dict["Nombre"] = "Pedro"
+print(my_dict["Nombre"])
+
+print(my_dict[1])
+
+my_dict["Calle"] = "Calle Dans"
+
+print(my_dict["Calle"])
+print(my_dict)
+
+del my_dict["Calle"]
+print(my_dict)
