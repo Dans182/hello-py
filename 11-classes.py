4b022c837aff8bb1e4901d6c95ac2caca7122b5e
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-11 13:19:08 +0200

    done

--- 11-classes.py
+++ 11-classes.py
@@ -9,15 +9,38 @@ class MyEmptyPerson: #Con las clases, lo correcto es definirlas, es en camel cas
 print(MyEmptyPerson) #Puede llamarse sin paréntesis
 print(MyEmptyPerson()) #o con paréntesis. De cualquier forma
 
-class Person: #Clase Persona/ Objeto Persona
-    def __init__(self, name, surname): #Esto (__init__) es reservado del sistema para crear un constructor de clase. Esto da la capacidad de que esta clase reciba algún parámetro. Siempre se llama self
+#class Person: #Clase Persona/ Objeto Persona
+#    def __init__(self, name, surname): #Esto (__init__) es reservado del sistema para crear un constructor de clase. Esto da la capacidad de que esta clase reciba algún parámetro. Siempre se llama self
         #Este def, no es propiamente una función, en este caso con el __init__ estamos haciendo un constructor de clase
-        pass
+#        pass
 
 
 #No está bien tener un fichero donde mezclemos funciones con clases. Lo correcto sería que tuviera un fichero que se llamara "Person" y dentro tengo la class Person y allí tengo sus métodos.
 
 print("###################################################################")
+
+class Person:
+    def __init__(self, name, surname): #El self es obligatorio, si queremos crear una clase que tenga un constructor, tenemos que pasar el self. Si estamos dentro de persona, se refiere a el mismo, a la clase Persona
+        self.name = name #el primer "name" es el parámetro, el segundo name es la variable 
+        self.surnameeee = surname #el primer "surname" es el parámetro, el segundo surname es la variable. Aca igualo
+
 my_person = Person("Daniel", "Gaiteiro")
 
-print(my_person)
+print(my_person.name)
+print(my_person.surnameeee) 
+
+print(f"{my_person.name} {my_person.surnameeee}")
+
+# class Person:
+#     def __init__(self):
+#         self.name = "Daniel"
+#         self.surnameeee = "Gaiteiro"  #Acá estoy definiendo el nombre y apellido dentro del constructor
+
+#Name y surname son PROPIEDADES de la clase
+
+class Person2:
+    def __init__(self, name, surname):
+        self.full_name = f"{my_person.name} {my_person.surnameeee}"
+
+my_person2 = Person2("Daniel", "Gaiteiro")
+print(my_person2.full_name)
