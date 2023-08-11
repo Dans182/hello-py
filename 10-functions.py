65fa5ed6a8744afe5974049007f7b748a112366b
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-11 11:08:27 +0200

    done

---
+++ 10-functions.py
@@ -0,0 +1,13 @@
+### Functions ###
+
+def my_function():
+    print("Esto es una función")
+
+my_function()
+
+def sum_two_values(first_number, second_number):
+    return print(first_number + second_number)
+
+sum_two_values(2,3)
+sum_two_values("2","3") #concatena strings
+sum_two_values(2.4,7.3)
