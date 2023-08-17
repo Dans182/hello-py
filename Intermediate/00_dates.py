c3e802d0ecdf2a3a9ead62b3643a4bd579f1f8bb
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-17 18:05:05 +0200

    dates

--- Intermediate/00_dates.py
+++ Intermediate/00_dates.py
@@ -1,3 +1,29 @@
 ### Dates ###
 
-import datetime
+#De esta manera tengo que importar el modulo datetime, y despues entrar en datetime de nuevo y despues el método. De la forma de abajo no
+# import datetime
+# now = datetime.datetime.now()
+# print(now)
+
+from datetime import datetime
+
+now = datetime.now()
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+    print(date.timestamp())
+
+print_date(now)
+
+year_2023 = datetime(2023,1,1)
+
+print(year_2023)
+
+
+    
+
