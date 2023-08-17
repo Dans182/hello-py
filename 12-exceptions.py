98a6927ed8349d143fd14494e9eda009259047ee
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-17 12:45:53 +0200

    done

--- 12-exceptions.py
+++ 12-exceptions.py
@@ -1,3 +1,13 @@
 ### Exception Handling ###
 # Manejo de errores #
 
+# try
+# except
+# else
+# finally
+
+number_one = 5
+number_two = 1
+#number_two = "1" Aca el programa me daría error, porque no se puede llevar a cabo esta suma, y se rompería la ejecución del programa
+
+print(number_one + number_two)
