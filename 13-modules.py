07e8327426ba419b35832b39611f16ae16b89edc
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-17 14:49:40 +0200

    done

---
+++ 13-modules.py
@@ -0,0 +1,26 @@
+### Modules ###
+
+# Un modulo es esa librería, ese fichero donde tenemos código que queremos utilizar.
+# Cada fichero se comporta como un módulo.
+
+#my_function() no funciona, porque en este fichero no existe.
+
+import module #Tengo que importar y posteriormente acceder
+# from 10-functions import sum_two_values Esta importación no funciona, porque Python no reconoce que el nombre de un fichero empiece llamándose con un número.
+
+module.sum(5, 3, 9)
+
+# Aca importo TODO lo que hay en module en mi programa, pero eso muchas veces es innecesario. No hace falta importar todo si solo necesitamos
+# una funcionalidad. Esto hace que mi programa se ralentice un poco mas
+# Estoy exponiendo este módulo hacia afuera.
+# Modulo tambien son librerías ya creadas por el propio sistema, el propio python pero que por defecto no tengamos acceso. Puede ser una librería creada por otra persona,
+# que nos la bajemos por github y la instalamos en nuestro proyecto y lo usamos en nuestra app.
+
+module.printValue("Hola, k ase?")
+
+print("############################")
+from module import sum
+
+sum(5, 3, 9)
+
+module.printValue("Hola, k ase?")
