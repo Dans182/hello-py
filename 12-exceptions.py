f3b849759a97ebadcda93cc88bfedeb39388db55
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-17 12:50:59 +0200

    done

--- 12-exceptions.py
+++ 12-exceptions.py
@@ -8,6 +8,14 @@
 
 number_one = 5
 number_two = 1
-#number_two = "1" Aca el programa me daría error, porque no se puede llevar a cabo esta suma, y se rompería la ejecución del programa
+number_two = "1" #Aca el programa me daría error, porque no se puede llevar a cabo esta suma, y se rompería la ejecución del programa
 
-print(number_one + number_two)
+#print(number_one + number_two)
+
+#Un if/else no es sustituible o se puede considerar un try/catch (en Python es Try/Except)
+
+try:
+    print(number_one + number_two)
+    print("No se ha producido un error")
+except:
+    print("Se ha producido un error")
