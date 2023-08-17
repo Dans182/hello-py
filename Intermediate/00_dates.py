c87255b27a79ebfd1e221eff352bb91c5634d1ee
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-17 19:45:48 +0200

    dates

--- Intermediate/00_dates.py
+++ Intermediate/00_dates.py
@@ -20,10 +20,26 @@ def print_date(date):
 
 print_date(now)
 
-year_2023 = datetime(2023,1,1)
+year_2023 = datetime(2023, 1, 1)
 
 print(year_2023)
 
+from datetime import time #Con esta librería basicamente rellenamos nosotros los campos de hora. En datetime lo extrae directamente de la hora local
 
+current_time = time(21, 6, 0)
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+
+from datetime import date #Con esta librería basicamente rellenamos nosotros los campos de fecha. En datetime lo extrae directamente de la hora local y agrupa hora y fecha
+
+#current_date = date(2025, 3, 31)
+current_date = date.today()
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
     
 
