ea7d822dcc553dbb08ccd47a6a13f48bdcfd4cb0
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-17 20:44:05 +0200

    lists comprehension done

--- 01_list_comprehension.py
+++ 01_list_comprehension.py
@@ -1,1 +1,28 @@
 ### List Comprehension ###
+
+#Esto es una forma de hacer listas comprimidas, o hacer listas de manera rápida
+
+my_original_list = [0, 1, 2, 3, 4, 5, 6, 7]
+print(my_original_list)
+
+
+my_list = [i for i in my_original_list]
+print(my_list)
+
+my_list = [i for i in range(7)]
+print(my_list)
+
+my_list = [i + 1 for i in range(7)]
+print(my_list)
+
+my_list = [i * 2 for i in range(7)]
+print(my_list)
+
+my_list = [i * i for i in range(7)]
+print(my_list)
+
+def sum_five(number):
+    return number + 5
+
+my_list = [sum_five(i) for i in range(7)]
+print(my_list)
