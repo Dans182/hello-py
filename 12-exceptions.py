baaf7fe9edaf05df702e05732d7cbba56d8c031a
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-17 12:57:16 +0200

    done

--- 12-exceptions.py
+++ 12-exceptions.py
@@ -8,7 +8,7 @@
 
 number_one = 5
 number_two = 1
-number_two = "1" #Aca el programa me daría error, porque no se puede llevar a cabo esta suma, y se rompería la ejecución del programa
+#number_two = "1" #Aca el programa me daría error, porque no se puede llevar a cabo esta suma, y se rompería la ejecución del programa
 
 #print(number_one + number_two)
 
@@ -19,3 +19,14 @@ try:
     print("No se ha producido un error")
 except:
     print("Se ha producido un error")
+
+print("#####################################################################")
+
+try:
+    print(number_one + number_two)
+    print("No se ha producido un error")
+except:
+    print("Se ha producido un error")
+else:
+    print("La ejecución continúa correctamente") #Si se produce una excepción, el else no se ejecuta.
+# El else en este caso se ejecuta, si las líneas del try se ejecutan correctamente y no redirecciona aun error.
