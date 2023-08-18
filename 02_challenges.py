8f21d5699f58395fe9ba6f3e107c213e04689d49
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-18 11:34:24 +0200

    challenges 2 done

--- 02_challenges.py
+++ 02_challenges.py
@@ -55,11 +55,9 @@ def fibonacci():
     next = 1
 
     for i in range(0, 50):
-        print(i)
         print(prev)
         fib = prev + next
         prev = next
         next = fib
-
 
 fibonacci()
