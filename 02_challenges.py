fe6a54ad46ef12a3d0db6ee788ddef9bcb2c2395
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-18 10:13:36 +0200

    challenges

---
+++ 02_challenges.py
@@ -0,0 +1,25 @@
+### Challenges  ###
+
+"""
+EL FAMOSO "FIZZ BUZZ”
+Escribe un programa que muestre por consola (con un print) los
+números de 1 a 100 (ambos incluidos y con un salto de línea entre
+cada impresión), sustituyendo los siguientes:
+- Múltiplos de 3 por la palabra "fizz".
+- Múltiplos de 5 por la palabra "buzz".
+- Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+"""
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 != 0 and i % 5 != 0 or i % 3 != 0 or i % 5 != 0:
+            print(i)
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        if i % 3 == 0:
+            print("fizz")
+        if i % 5 == 0:
+            print("buzz")
+
+
+fizzbuzz()
