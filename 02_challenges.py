8893139b08a2be53d3bf1b4664dec4edc1ab729c
Daniel Gaiteiro <dgaiteiro@gmail.com>
2023-08-18 10:50:19 +0200

    challenges 2 done

--- 02_challenges.py
+++ 02_challenges.py
@@ -1,7 +1,7 @@
 ### Challenges  ###
 
 """
-EL FAMOSO "FIZZ BUZZ”
+Reto #0: EL FAMOSO "FIZZ BUZZ”
 Escribe un programa que muestre por consola (con un print) los
 números de 1 a 100 (ambos incluidos y con un salto de línea entre
 cada impresión), sustituyendo los siguientes:
@@ -22,3 +22,20 @@ def fizzbuzz():
             print(i)
 
 fizzbuzz()
+
+"""
+Reto #1: ¿ES UN ANAGRAMA?
+Escribe una función que reciba dos palabras (String) y retorne
+verdadero o falso (Bool) según sean o no anagramas.
+- Un Anagrama consiste en formar una palabra reordenando TODAS
+  las letras de otra palabra inicial.
+- NO hace falta comprobar que ambas palabras existan.
+- Dos palabras exactamente iguales no son anagrama.
+"""
+
+def is_anagram(word_one, word_two):
+    if word_one.lower() == word_two.lower(): #Este if evalua directamente si NO es un anagrama. Si estás ante una palabra idéntica
+        return False, "No es un anagrama"
+    return sorted(word_one.lower()) == sorted(word_two.lower()) #Acá si evalua directamente si es un anagrama.
+
+print(is_anagram("Amor", "Roma"))
